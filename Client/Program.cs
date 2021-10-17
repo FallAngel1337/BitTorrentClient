1c2ce004df9c969821916537ba1d733300f40791
FallAngel1337 <fallangel@protonmail.com>
2021-10-17 15:08:26 -0300

    Added the options setup and using MonoTorrent library

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,23 @@
 ﻿using System;
+using CommandLine;
+using System.Threading.Tasks;
+using MonoTorrent;
+using MonoTorrent.Client;
 
 namespace Client
 {
+	public class Options
+	{
+		[Option('v', "verbose", Required = false, HelpText = "Enable verbose mode")]
+		public bool Verbose { get; set; } = false;
+
+		[Option('l', "load", Required = true, HelpText = "The torrent file/link/hash")]
+		public string Torrent { get; set; } = null;
+
+		[Option('d', "path", Required = false, HelpText = "Download path")]
+		public string Download { get; set; } = Environment.CurrentDirectory;
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
